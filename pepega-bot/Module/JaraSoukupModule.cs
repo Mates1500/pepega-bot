ef54c921abce4d2fe8bbdf694328abf077911afa
Martin Pecánek <mates1500@gmail.com>
2020-01-01 16:06:58 +0100

    add JaraSoukup module

---
+++ pepega-bot/Module/JaraSoukupModule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Discord;
+using Microsoft.Extensions.Configuration;
+using pepega_bot.Services;
+using pepega_bot.Utils;
+
+namespace pepega_bot.Module
+{
+    internal class JaraSoukupModule
+    {
+        private readonly IConfiguration _config;
+        private readonly BaseWarnMessageModule _baseWarnMessageModule;
+        private readonly string _jaraSoukupUserId;
+        private readonly Emoji _rabbit2Emoji;
+
+
+        public JaraSoukupModule(IConfigurationService configService, CommandHandlingService chService)
+        {
+            _config = configService.Configuration;
+            _jaraSoukupUserId = _config["UserIds:JaraSoukup"];
+            _rabbit2Emoji = new Emoji(_config["Emojis:Rabbit2"]);
+            var blueCheckmark = new Emoji(_config["Emojis:BlueCheckmark"]);
+
+            _baseWarnMessageModule = new BaseWarnMessageModule(blueCheckmark);
+
+            chService.ReactAdded += ReactionAddedAsync;
+        }
+
+        private async void ReactionAddedAsync(object sender, ReactionAddedEventArgs e)
+        {
+            if (!e.React.Emote.Equals(_rabbit2Emoji)) return;
+
+            var reactInfo = new ReactInfo(_config, e.Message, e.Channel, e.React);
+            var warnMessage =
+                $"Master <@{_jaraSoukupUserId}>, your comrade <@{reactInfo.ReactAuthorId}> has found some *steamy* discussion. You should take a look." +
+                Environment.NewLine + $"Discussion reference: {reactInfo.MessageLink}";
+
+            await _baseWarnMessageModule.WarnMessage(e.Message, e.Channel, e.React, warnMessage);
+        }
+    }
+}
