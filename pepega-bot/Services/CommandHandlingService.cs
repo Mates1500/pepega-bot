3242a838c63bfdebbb0e275f6868068cfd8af31d
Martin Pecánek <mates1500@gmail.com>
2022-04-27 19:58:07 +0200

    use actual Tasks in CommandHandlingService instead of flooding the Gateway Task

--- pepega-bot/Services/CommandHandlingService.cs
+++ pepega-bot/Services/CommandHandlingService.cs
@@ -33,41 +33,66 @@ namespace pepega_bot.Services
         private Task ReactionAddedAsync(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel, SocketReaction reaction)
         {
             var tempCopy = ReactAdded;
+            if(tempCopy == null)
+                return Task.CompletedTask;
+
             var args = new ReactionAddedEventArgs(message, channel, reaction);
-            tempCopy?.Invoke(this, args);
-            return Task.CompletedTask;
+            return Task.Run(() =>
+            {
+                tempCopy.Invoke(this, args);
+            });
         }
 
         private Task ReactionRemovedAsync(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel, SocketReaction reaction)
         {
             var tempCopy = ReactRemoved;
+            if (tempCopy == null)
+                return Task.CompletedTask;
+
             var args = new ReactionRemovedEventArgs(message, channel, reaction);
-            tempCopy?.Invoke(this, args);
-            return Task.CompletedTask;
+            return Task.Run(() =>
+            {
+                tempCopy.Invoke(this, args);
+            });
         }
 
         private Task MessageUpdatedAsync(Cacheable<IMessage, ulong> oldMessage, SocketMessage newMessage, ISocketMessageChannel channel)
         {
             var tempCopy = MessageUpdated;
+            if (tempCopy == null)
+                return Task.CompletedTask;
+
             var args = new MessageUpdatedEventArgs(oldMessage, newMessage, channel);
-            tempCopy?.Invoke(this, args);
-            return Task.CompletedTask;
+            return Task.Run(() =>
+            {
+                tempCopy.Invoke(this, args);
+            });
         }
 
         private Task MessageReceivedAsync(SocketMessage message)
         {
             var tempCopy = MessageReceived;
+            if (tempCopy == null)
+                return Task.CompletedTask;
+
             var args = new MessageReceivedEventArgs(message);
-            tempCopy?.Invoke(this, args);
-            return Task.CompletedTask;
+            return Task.Run(() =>
+            {
+                tempCopy.Invoke(this, args);
+            });
         }
 
         private Task MessageDeletedAsync(Cacheable<IMessage, ulong> message, ISocketMessageChannel channel)
         {
             var tempCopy = MessageRemoved;
+            if (tempCopy == null)
+                return Task.CompletedTask;
+
             var args = new MessageRemovedEventArgs(message, channel);
-            tempCopy?.Invoke(this, args);
-            return Task.CompletedTask;
+            return Task.Run(() =>
+            {
+                tempCopy.Invoke(this, args);
+            });
         }
 
         public async Task InitializeAsync()
